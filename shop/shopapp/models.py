4b823408c6a11d3b425473da77328ebf9488e752
hokole <94093120+hokole@users.noreply.github.com>
2023-09-28 15:23:14 -0500

    Update models.py

--- shop/shopapp/models.py
+++ shop/shopapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    url = models.CharField(max_length=1000)
+    img = models.CharField(max_length=1000)
+    title = models.CharField(max_length=1000)
+    price = models.CharField(max_length=1000)
+    user = models.CharField(max_length=1000)
+
+    def __str__(self):
+            return self.image
