5adfb9cf6e13f153f43538c297aec88dc8238a45
hokole <94093120+hokole@users.noreply.github.com>
2023-09-28 15:44:20 -0500

    Print bs images

--- shop/shopapp/views.py
+++ shop/shopapp/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+import requests
+from bs4 import BeautifulSoup as bs
 
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        url_link = request.POST['url_link']
+        r = requests.get(url_link)
+        soup = bs(r.content)
+        images = soup.find('img')['src']
+        print(images)
+
     return render(request, 'home.html')
 
 def logout_view(request):
