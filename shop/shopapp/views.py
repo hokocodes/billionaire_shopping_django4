cc11e00ae45d7949dfeed8509e569222da134bf8
hokole <94093120+hokole@users.noreply.github.com>
2023-09-29 15:05:48 -0500

    Scraped amazon

--- shop/shopapp/views.py
+++ shop/shopapp/views.py
@@ -6,17 +6,25 @@ from .models import Item
 
 # Create your views here.
 
+HEADERS = ({'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
+
 def home(request):
     if request.method == 'POST':
-        url_link = request.POST['url_link']
-        r = requests.get(url_link)
-        soup = bs(r.content)
-        images = soup.find_all('img')
-        imagess = []
-        for image in images:
-            imagess.append(image['src'])
+        try: 
+            url_link = request.POST['url_link']
+            r = requests.get(url_link, headers=HEADERS)
+            soup = bs(r.content, "lxml")
+            images = soup.find_all('img')
+            images2 = []
+            for image in images:
+                images2.append(image['src'])
+                print(image['src'])
+        except: 
+            pass
 
-    return render(request, 'home.html', {'images': imagess})
+    return render(request, 'home.html', {'images': images2})
 
 def logout_view(request):
     logout(request)
